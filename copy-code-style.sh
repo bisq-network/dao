e61eab61cb381c668ef5ad4487470140c73139b2
Chris Beams <chris@beams.io>
2018-03-16 11:26:23 +0100

    Add .editorconfig to copy-code-style.sh

--- copy-code-style.sh
+++ copy-code-style.sh
@@ -2,3 +2,4 @@
 
 cd $(dirname $0)
 find . -mindepth 3 -type d -name codeStyles | xargs -I dir cp -v .idea/codeStyles/* dir/
+find . -mindepth 2 -type f -name .editorconfig | xargs -I file cp -v .editorconfig file
